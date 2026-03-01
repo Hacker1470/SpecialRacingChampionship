cb2b441921bc7ec4fef675b03dcaa9f11318a483
Mikhail <160272236+Hacker1470@users.noreply.github.com>
2026-03-02 01:34:20 +0300

    New class for parts storaging
    Now all parts will be at here. Also here some helper methods to work with all lists

---
+++ src/data/partslists/CatalogOfParts.java
@@ -0,0 +1,44 @@
+package data.partslists;
+
+import vehicle.Engine;
+import vehicle.Part;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CatalogOfParts {
+
+    public static HashMap<String, Engine> allCatalog = new HashMap<>();
+    public static HashMap<String, Engine> engineCatalog = new HashMap<>();
+
+    public static void catalogInit(){
+        initializeEngines();
+    }
+
+    private static void initializeEngines(){
+        engineCatalog.put(EnginesList.pedal.getId(), EnginesList.pedal);
+        engineCatalog.put(EnginesList.lada.getId(), EnginesList.lada);
+        engineCatalog.put(EnginesList.sigma.getId(), EnginesList.sigma);
+
+        allCatalog.put(EnginesList.pedal.getId(), EnginesList.pedal);
+        allCatalog.put(EnginesList.lada.getId(), EnginesList.lada);
+        allCatalog.put(EnginesList.sigma.getId(), EnginesList.sigma);
+    }
+
+    public static HashMap<Integer, Part> getAvailableByReputation(PartType type, int rep){
+        HashMap<Integer, Part> availableParts = new HashMap<>();
+
+        int counter = 0;
+        for (Part i : allCatalog.entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith(type.getId()))
+                .map(Map.Entry::getValue)
+                .toList()){
+            if(i.getReputationLevel() <= rep){
+                availableParts.put(++counter, i);
+            }
+        }
+
+        return availableParts;
+    }
+}
