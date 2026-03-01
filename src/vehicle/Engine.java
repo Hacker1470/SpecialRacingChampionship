10f25b62ca8b04b972e582dedb3fd70c82d42d30
Mikhail <160272236+Hacker1470@users.noreply.github.com>
2026-03-02 01:25:51 +0300

    Update Engine.java
    Added getStringOfCharacteristics which generate a string for market tabs
    Also constructor was modified for Part class update.

--- src/vehicle/Engine.java
+++ src/vehicle/Engine.java
@@ -1,5 +1,9 @@
 package vehicle;
 
+import data.partslists.ConnectivitySearcher;
+import data.partslists.PartType;
+import ui.ConsoleControl;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +14,7 @@ public class Engine extends Part {
 
     public Engine(String id, String name, int stockPrice, int quality, int mass, int damage,
                   int reputationLevel, List<String> connectivity, int power, int maxRpm, int fuelConsumption){
-        super(id, name, stockPrice, quality, mass, damage, reputationLevel, connectivity);
+        super(PartType.ENGINE, id, name, stockPrice, quality, mass, damage, reputationLevel, connectivity);
 
         this.power = power;
         this.maxRpm = maxRpm;
@@ -32,6 +36,29 @@ public class Engine extends Part {
     @Override
     public int getRealPrice(){
         return stockPrice * (damage + quality) / 100;
+    }
+
+    @Override
+    public String getStringOfCharacteristics(){
+        StringBuilder sb = new StringBuilder(2000);
+
+        sb.append("Название: ").append(getName()).append("\n");
+        sb.append("Качество: ").append(getQuality()).append("\n");
+        sb.append("Масса: ").append(getMass()).append(" кг").append("\n");
+        sb.append("Износ: ").append(getName()).append(" %").append("\n");
+        sb.append("Мощность: ").append(getPower()).append(" л/с").append("\n");
+        sb.append("Максимальные обороты: ").append(getMaxRpm()).append(" об/мин").append("\n");
+        sb.append("Потребление топлива: ").append(getName()).append(" литров/с").append("\n");
+        sb.append("\n");
+        sb.append("Совместимость:\n");
+
+        for (Part i : ConnectivitySearcher.findAllConnectivities(chosenEngine)){
+            ConsoleControl.printlnString("* " + i.getName());
+
+        }
+
+        ConsoleControl.printlnString("Стоимость: " + chosenEngine.getRealPrice());
+        return sb.toString();
     }
 
     @Override
