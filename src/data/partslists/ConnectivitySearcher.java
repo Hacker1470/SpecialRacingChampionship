4da266f76c42b636ae03505bcab47e525dc8e5bb
Mikhail <160272236+Hacker1470@users.noreply.github.com>
2026-03-02 01:29:30 +0300

    Update ConnectivitySearcher.java
    IDK what to do with it. I think i will delete this later

--- src/data/partslists/ConnectivitySearcher.java
+++ src/data/partslists/ConnectivitySearcher.java
@@ -20,27 +20,30 @@ public class ConnectivitySearcher {
         return response;
     }
 
-    public static List<Part> findConnectivities(Part part, Class<Part> requiredType){
-        List<Part> response = new ArrayList<>();
 
-        //Закончил здесь. Основная мысль: мне в карточке детали нужно по отдельности выводить, с какими деталями
-        //совместима данная деталь. Например, хочу узнать с какими коробками совместим движок, и он мне должен
-        //на основе списка part.connectivity и класса вернуть список с id-шниками деталей нужного типа
-        String targetId;
-        if(Engine.class instanceof requiredType.g){
+//    public static List<Part> findConnectivities(Part part, Class<Part> requiredType){
+//        List<Part> response = new ArrayList<>();
+//
+//        //Закончил здесь. Основная мысль: мне в карточке детали нужно по отдельности выводить, с какими деталями
+//        //совместима данная деталь. Например, хочу узнать с какими коробками совместим движок, и он мне должен
+//        //на основе списка part.connectivity и класса вернуть список с id-шниками деталей нужного типа
+          //upd 01 03 2026: можно, а зачем?
 
-        }
-
-        switch (requiredType.getClass().){
-            case:
-
-        }
-        for(String i : part.getConnectivity()){
-            if(i.startsWith("tran") && TransmissionList.listOfTransmission.get(i) != null){
-                response.add(TransmissionList.listOfTransmission.get(i));
-            }
-        }
-
-        return response;
-    }
+//        String targetId;
+//        if(Engine.class instanceof requiredType.g){
+//
+//        }
+//
+//        switch (requiredType.getClass().){
+//            case:
+//
+//        }
+//        for(String i : part.getConnectivity()){
+//            if(i.startsWith("tran") && TransmissionList.listOfTransmission.get(i) != null){
+//                response.add(TransmissionList.listOfTransmission.get(i));
+//            }
+//        }
+//
+//        return response;
+//    }
 }
