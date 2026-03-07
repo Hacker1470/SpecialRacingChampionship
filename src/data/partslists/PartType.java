f72afa767d16ac91d8f1f255b3d8206fa305dc47
Mikhail <160272236+Hacker1470@users.noreply.github.com>
2026-03-07 14:50:55 +0300

    Update PartType.java
    Now it stores titles for market tabs

--- src/data/partslists/PartType.java
+++ src/data/partslists/PartType.java
@@ -1,17 +1,59 @@
 package data.partslists;
 
 public enum PartType {
-    ENGINE("engi"),
-    TRANSMISSION("tran"),
-    ANY("");
+    UNDEF(
+        "????",
+        "====== ??? ======\nВам доступно:",
+        "====== ПОДРОБНЕЕ ======"
+    ),
+    ENGINE(
+        "engi",
+        "====== ДВИГАТЕЛИ ======\nВам доступны следующие двигатели:",
+        "====== ПОДРОБНЕЕ О ДВИГАТЕЛЕ ======"
+    ),
+    TRANSMISSION(
+        "tran",
+        "====== ТРАНСМИССИЯ ======\nВам доступны следующие КПП:",
+        "====== ПОДРОБНЕЕ О ТРАНСМИСИИ ======"
+    ),
+    SUSPENSION(
+        "susp",
+        "====== ПОДВЕСКА ======\nВам доступны следующие подвески:",
+        "====== ПОДРОБНЕЕ О ПОДВЕСКЕ ======"
+    ),
+    CHASSIS(
+        "chas",
+        "====== ШАССИ ======\nВам доступны следующие шасси:",
+        "====== ПОДРОБНЕЕ О ШАССИ ======"
+    ),
+    DOWNFORCE(
+        "dwfr",
+        "====== ПРИЖИМНЫЕ ДЕТАЛИ ======\nВам доступны следующие усилители прижимной силы:",
+        "====== ПОДРОБНЕЕ О ПРИЖИМНЫХ ДЕТАЛЯХ ======"
+    ),
+    WHEELS(
+        "whel",
+        "====== КОЛЁСА ======\nВам доступны следующие колёса:",
+        "====== ПОДРОБНЕЕ О КОЛЁСАХ ======"
+    );
 
-
-    PartType(String id){
+    PartType(String id, String marketGroupTitle, String marketInfoTitle){
         this.id = id;
-    };
+        this.marketGroupTitle = marketGroupTitle;
+        this.marketInfoTitle = marketInfoTitle;
+    }
+
     private final String id;
+    private final String marketGroupTitle;
+    private final String marketInfoTitle;
 
     public String getId(){
         return id;
+    }
+    public String getMarketGroupTitle(){
+        return marketGroupTitle;
+    }
+    public String getMarketInfoTitle(){
+        return marketInfoTitle;
     }
 }
