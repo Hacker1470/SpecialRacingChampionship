960e88a69194c49e99771e9b6620a2ce11e7a4db
Mikhail <160272236+Hacker1470@users.noreply.github.com>
2026-03-09 17:07:41 +0300

    Update Main.java
    For new employees

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
-import data.GameSession;
-import data.partslists.CatalogOfParts;
-import data.partslists.EnginesList;
-import data.partslists.TransmissionList;
-import ui.MainMenu;
-import vehicle.Part;
-import vehicle.Wheels;
+import data.CatalogOfEmployees;
+import game.GameSession;
+import data.CatalogOfParts;
 
 void main() {
-    GameSession gm = new GameSession(0, 1000);
+    GameSession gm = new GameSession(10000, 1000);
     CatalogOfParts.catalogInit();
-    //TransmissionList.initializeTransmission();
+    CatalogOfEmployees.catalogInit();
     gm.launch();
+
+    //Заметки на будущее
+    //Мне ведь надо как-то идентифицировать каждую деталь и человека на складе и в общаге, чтобы я мог изменять
+    //их характеристики.
+    //Предлагаю при покупке в айдишник добавлять им порядковый номер. Когда интеджер дойдёт до своего края,
+    //нумерация начнётся заново, причём должна быть ещё проверка на то, чтобы айдишники не повторялись.
+    //Либо может в класс добавить отдельное поля, я не знаю
 }
