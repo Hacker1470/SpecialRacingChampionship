19e4b57a546ca1552729b4b9da6600bc2605a025
Mikhail <160272236+Hacker1470@users.noreply.github.com>
2026-03-24 18:43:21 +0300

    Update ConsoleControl.java

--- src/ui/handling/ConsoleControl.java
+++ src/ui/handling/ConsoleControl.java
@@ -1,5 +1,11 @@
 package ui.handling;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class ConsoleControl {
     public static void clear(){
         IO.println("");
@@ -7,6 +13,10 @@ public class ConsoleControl {
         IO.println("");
     }
 
+    /**
+     * не работает с русским вводом
+     * @return
+     */
     public static String getString(){
         return IO.readln();
     }
