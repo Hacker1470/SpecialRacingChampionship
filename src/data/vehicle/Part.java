488a00636a4cb5e4b5ba3c6acf1b71ee82449d64
Mikhail <160272236+Hacker1470@users.noreply.github.com>
2026-03-24 18:39:43 +0300

    Postfix added & new set methods & lot of descriptions

--- src/data/vehicle/Part.java
+++ src/data/vehicle/Part.java
@@ -7,17 +7,56 @@ public abstract class Part {
     /*
     Я бы заменил damage, connectionReliability и quality на тип byte, а mass на тип short
     */
-    private Long id;
-    private PartType type;
-    private String article;
-    private String name;
-    private int stockPrice;
-    private int quality;
-    private int mass;
+
+    /**
+     * Уникальный идентификатор, используемый в логике игры для дифференцирования деталей
+     */
+    private final Long id;
+
+    /**
+     * Тип детали
+     */
+    private final PartType type;
+
+    /**
+     * Уникальное короткое имя, обозначающее принадлежность детали к своему виду
+     */
+    private final String article;
+
+    /**
+     * Собственное имя детали
+     */
+    private final String name;
+
+    /**
+     * Приписка после основного имени, применяющаяся для дифференцирования детали
+     * на уровне восприятия интерфейса игроком
+     */
+    private String postfix;
+
+    /**
+     * Базовая цена, от которой с помощи множителей формируется реальная цена
+     */
+    private final int stockPrice;
+
+    private final int quality;
+    private final int mass;
     private int damage;
+
+    /**
+     * Качество соединения. Зависит от мастерства инженера
+     */
     private int connectionReliability;
-    private int reputationLevel;
-    private List<String> connectivity;
+
+    /**
+     * Уровень репутации, начиная с которого деталь может появиться в магазине
+     */
+    private final int reputationLevel;
+
+    /**
+     * Список из строк формата article для выявления совместимости с другими деталями
+     */
+    private final List<String> connectivity;
 
     public Part(Long id, PartType type, String article, String name, int stockPrice, int quality, int mass, int damage,
                 int reputationLevel, List<String> connectivity){
@@ -25,6 +64,7 @@ public abstract class Part {
         this.type = type;
         this.article = article;
         this.name = name;
+        this.postfix = "";
         this.stockPrice = stockPrice;
         this.quality = quality;
         this.mass = mass;
@@ -33,6 +73,12 @@ public abstract class Part {
         this.reputationLevel = reputationLevel;
         this.connectivity = new ArrayList<>();
         this.connectivity.addAll(connectivity);
+    }
+
+    public Part(Long id, PartType type, String article, String name, String postfix, int stockPrice, int quality, int mass, int damage,
+                int reputationLevel, List<String> connectivity){
+        this(id, type, article, name, stockPrice, quality, mass, damage, reputationLevel,connectivity);
+        this.postfix = postfix;
     }
 
     public Long getId() {
@@ -46,6 +92,9 @@ public abstract class Part {
     }
     public String getName(){
         return name;
+    }
+    public String getPostfix(){
+        return postfix;
     }
     public int getStockPrice(){
         return stockPrice;
@@ -69,12 +118,21 @@ public abstract class Part {
         return connectivity;
     }
 
+    public void setPostfix(String newValue){
+        postfix = newValue;
+    }
+    public void setDamage(int newValue){
+        damage = newValue;
+    }
+    public void setConnectionReliability(int newValue){
+        connectionReliability = newValue;
+    }
 
     public abstract String getStringOfCharacteristics();
     /**
-     * только для маркет
+     * Применяется для копирования детали из каталога на склад игрока
      * @param idNew
-     * @return Copy of part with new ID
+     * @return Копия детали с новым id = idNew
      */
     public abstract Part getCopy(Long idNew);
     public abstract int getRealPrice();
